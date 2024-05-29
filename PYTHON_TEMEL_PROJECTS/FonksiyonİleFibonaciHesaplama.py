a48b74ec82ccf0862c3c42cd782f55046fa571f2
emirkacar <emirkacar86@gmail.com>
2024-05-29 13:48:57 +0300

    Add files via upload

---
+++ PYTHON_TEMEL_PROJECTS/FonksiyonİleFibonaciHesaplama.py
@@ -0,0 +1,14 @@
+def fib(sayi):
+    a = 1
+    b = 1
+    print(a)
+    print(b)
+    for i in range(1,14):
+        c = a+b
+        a = b
+        b = c
+        print(c)
+    
+fib(13)
+        
+
